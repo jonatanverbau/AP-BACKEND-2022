4c70cb53ad4072ce316acb6465d17fa9e84107ec
Jonatan <jonatanverbau@gmail.com>
2022-06-02 22:47:29 -0300

    v

--- src/main/java/com/portfolio/jnv/JnvApplication.java
+++ src/main/java/com/portfolio/jnv/JnvApplication.java
@@ -1,12 +1,12 @@
 package com.portfolio.jnv;
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class JnvApplication {
